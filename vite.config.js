796be84e9b33d25550ce8291e169f210fd46d19f
MyBrightByte <110934993+OhWire@users.noreply.github.com>
2024-07-05 14:23:58 +0100

    3d

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    open: true, // Opens the browser automatically
+  },
+  build: {
+    outDir: 'dist', // Output directory for the build
+  },
+});
