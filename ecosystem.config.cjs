b4a41f62893999d177ff013bcd086d0556996619
Chris Breunig <chrisbreunig2311@gmail.com>
2024-07-06 19:47:15 +0200

    server zusammenfügen test

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,16 +1,37 @@
-
 module.exports = {
-    apps: [
-      {
-        name: `vite-app`,
-        script: "serve",
-        env: {
-          PM2_SERVE_PATH: "./dist",
-          PM2_SERVE_PORT: 5173,
-          PM2_SERVE_SPA: "true",
-          NODE_ENV: 'production',
-        },
+  apps: [
+    {
+      name: "login",
+      script: "./api/login.js",
+      watch: true,
+      env: {
+        NODE_ENV: "development",
       },
-    ],
-  };
-  
+    },
+    {
+      name: "register",
+      script: "./api/register.js",
+      watch: true,
+      env: {
+        NODE_ENV: "development",
+      },
+    },
+    {
+      name: "confirm",
+      script: "./api/confirm.js",
+      watch: true,
+      env: {
+        NODE_ENV: "development",
+      },
+    },
+    {
+      name: "react-app",
+      script: "npm",
+      args: "start",
+      cwd: "./client",
+      env: {
+        NODE_ENV: "development",
+      },
+    },
+  ],
+};
